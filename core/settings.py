8e95ebf8e67f213da7db30ea19c8056b42bae77c
shubhamtit <shubhamsinghtit@gmail.com>
2026-03-28 22:28:11 +0530

    v1

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,4 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Ensure BASE_DIR is properly defined
+
+# Enable WhiteNoise to serve static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
